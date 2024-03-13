7d58557918d0cdeccb59470412f8092d3ce1ee28
Thais Kotovicz <thais_kotovicz@hotmail.com>
2024-03-13 12:05:08 -0300

    feat: create auth middleware

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from "express";
+import { ErrorTypes } from "../errors/catalog";
+import JwtService from "../utils/jwt";
+
+export default function authMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    throw new Error(ErrorTypes.InvalidToken);
+  }
+
+  const [token] = authorization.split(" ");
+
+  try {
+    JwtService.validateToken(token);
+    return next();
+  } catch (error) {
+    throw new Error(ErrorTypes.InvalidToken);
+  }
+}
